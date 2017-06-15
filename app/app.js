4bac73cdc1821921f8ec7615c2f718413cbbd5ba
Kevin Jang <pianistkevinjang@gmail.com>
2017-06-15 12:16:24 -0700

    Move hardcoded data into service

--- app/app.js
+++ app/app.js
@@ -1,66 +1,42 @@
 var app = angular.module('contactList', ['ui.bootstrap']);
 
-app.controller('mainController', ['$scope', function($scope) {
+app.controller('mainController', ['$scope', 'contacts', function($scope, contacts) {
 
   // Handle pagination
   $scope.totalItems = 0;
   $scope.currentPage = 1;
-
-  $scope.setPage = function(pageNum) {
-    $scope.currentPage = pageNum;
-  };
 
   $scope.pageChanged = function() {
     var start = $scope.currentPage * 10 - 1;
     $scope.currentPageContacts = $scope.contacts.slice(start, start + 10);
   };
 
-  // List of all contacts
-  $scope.contacts = [
-    {
-      _id: '107f1f77bcf86cd799439011',
-      firstName: 'Kevin',
-      lastName: 'Jang',
-      email: 'kevin.jang.swe@gmail.com',
-      phone: '7325898089',
-      date: new Date(1900, 1, 1),
-      groups: { family: true, friend: true, colleague: true, associate: true },
-      comments: 'Bacon ipsum dolor amet picanha leberkas turkey pork belly shank prosciutto kielbasa doner. Capicola turducken ribeye, andouille tenderloin leberkas shoulder frankfurter corned beef beef brisket cow porchetta meatloaf.'
-    },
-    {
-      _id: '207f1f77bcf86cd799439011',
-      firstName: 'Kevin',
-      lastName: 'Jang',
-      email: 'kevin.jang.swe@gmail.com',
-      phone: '7325898089',
-      date: new Date(1900, 1, 1),
-      groups: { family: true, friend: true, colleague: true, associate: true },
-      comments: 'Bacon ipsum dolor amet picanha leberkas turkey pork belly shank prosciutto kielbasa doner. Capicola turducken ribeye, andouille tenderloin leberkas shoulder frankfurter corned beef beef brisket cow porchetta meatloaf.'
-    },
-    {
-      _id: '307f1f771rhse86cd799439011',
-      firstName: 'Kevin',
-      lastName: 'Jang',
-      email: 'kevin.jang.swe@gmail.com',
-      phone: '7325898089',
-      date: new Date(1900, 1, 1),
-      groups: { family: true, friend: true, colleague: true, associate: true },
-      comments: 'Bacon ipsum dolor amet picanha leberkas turkey pork belly shank prosciutto kielbasa doner. Capicola turducken ribeye, andouille tenderloin leberkas shoulder frankfurter corned beef beef brisket cow porchetta meatloaf.'
-    },
-    {
-      _id: '407f1f77bcf86c231439011',
-      firstName: 'Kevin',
-      lastName: 'Jang',
-      email: 'kevin.jang.swe@gmail.com',
-      phone: '7325898089',
-      date: new Date(1900, 1, 1),
-      groups: { family: true, friend: true, colleague: true, associate: true },
-      comments: 'Bacon ipsum dolor amet picanha leberkas turkey pork belly shank prosciutto kielbasa doner. Capicola turducken ribeye, andouille tenderloin leberkas shoulder frankfurter corned beef beef brisket cow porchetta meatloaf.'
+  $scope.getGroups = function(contactGroups) {
+    var groups = [];
+
+    for (var group in contactGroups) {
+      if (contactGroups[group]) {
+        groups.push(group);
+      }
     }
-  ];
+
+    return groups.join(', ');
+  }
+
+  // List of all contacts
+  $scope.contacts = [];
 
   // List of current page contacts
-  $scope.currentPageContacts = [
-  ];
+  $scope.currentPageContacts = [];
+
+  // Get initial contacts
+  contacts.getContacts()
+    .then(function(contacts) {
+      $scope.$apply(function() {
+        $scope.totalItems = contacts.length;
+        $scope.contacts = contacts;
+        $scope.currentPageContacts = contacts;
+      });
+    });
 
 }]);
---
+++ app/services/contacts.js
@@ -0,0 +1,62 @@
+app.factory('contacts', function() {
+  var helpers = {};
+
+  helpers.getContacts = function() {
+    console.log('getContacts!');
+    return new Promise(function(resolve) {
+      resolve(
+        [
+          {
+            _id: '107f1f77bcf86cd799439011',
+            firstName: 'Kevin',
+            lastName: 'Jang',
+            email: 'kevin.jang.swe@gmail.com',
+            phone: '',
+            birthdate: null,
+            profile_picture: 'https://thumbs.dreamstime.com/z/giraffe-mother-baby-9749061.jpg',
+            groups: { family: true, friend: false, colleague: true, associate: true },
+            comments: 'Bacon ipsum dolor amet picanha leberkas turkey pork belly shank prosciutto kielbasa doner. Capicola turducken ribeye, andouille tenderloin leberkas shoulder frankfurter corned beef beef brisket cow porchetta meatloaf.'
+          },
+          {
+            _id: '207f1f77bcf86cd799439011',
+            firstName: 'Kevin',
+            lastName: 'Jang',
+            email: 'kevin.jang.swe@gmail.com',
+            phone: '7325898089',
+            birthdate: new Date(1900, 1, 1),
+            profile_picture: 'http://style.anu.edu.au/_anu/4/images/placeholders/person.png',
+            groups: { family: true, friend: false, colleague: true, associate: false },
+            comments: ''
+          },
+          {
+            _id: '307f1f771rhse86cd799439011',
+            firstName: 'Kevin',
+            lastName: 'Jang',
+            email: 'kevin.jang.swe@gmail.com',
+            phone: '7325898089',
+            birthdate: null,
+            profile_picture: 'http://style.anu.edu.au/_anu/4/images/placeholders/person.png',
+            groups: { family: false, friend: false, colleague: false, associate: false },
+            comments: 'Bacon ipsum dolor amet picanha leberkas turkey pork belly shank prosciutto kielbasa doner. Capicola turducken ribeye, andouille tenderloin leberkas shoulder frankfurter corned beef beef brisket cow porchetta meatloaf.'
+          },
+          {
+            _id: '407f1f77bcf86c231439011',
+            firstName: 'Kevin',
+            lastName: 'Jang',
+            email: 'kevin.jang.swe@gmail.com',
+            phone: '7325898089',
+            birthdate: new Date(1900, 1, 1),
+            profile_picture: 'http://style.anu.edu.au/_anu/4/images/placeholders/person.png',
+            groups: { family: false, friend: true, colleague: true, associate: true },
+            comments: 'Capicola turducken ribeye, andouille tenderloin leberkas shoulder frankfurter corned beef beef brisket cow porchetta meatloaf.'
+          }
+        ]
+      );
+    });
+  }
+  helpers.getContact = function(id) {}
+  helpers.addContact = function(contact) {}
+  helpers.editContact = function(contact) {}
+
+  return helpers;
+});
