09673a676302029f7e935d0cf377e3d88bf17bbc
Kevin Jang <pianistkevinjang@gmail.com>
2017-06-15 10:40:06 -0700

    Create contact list mockup view

---
+++ app.js
@@ -0,0 +1,29 @@
+var app = angular.module('contactList', ['ngResource']);
+
+app.controller('mainController', ['$scope', '$filter', function($scope, $filter) {
+
+  $scope.name = 'Kevin';
+  $scope.characters = 5;
+  $scope.rules = [
+    { rulename: 'Hello' },
+    { rulename: 'Huh' },
+    { rulename: 'Foo' }
+  ];
+
+  $scope.lowercaseName = function() {
+    return $filter('lowercase')($scope.name);
+  };
+
+  $scope.$watch('name', function(newValue, oldValue) {
+
+    console.info('changed!');
+    console.log('Old: ', oldValue);
+    console.log('New: ', newValue);
+
+  });
+
+  $scope.alertClick = function() {
+    alert('click!');
+  }
+
+}]);
